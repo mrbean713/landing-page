5d9aa282f5d8b6ab263348330356e09417469ab9
cartercote <cartercote06@gmail.com>
2025-01-28 03:32:08 -0800

    new styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import Image from "next/image";
+import { AnimatedGradientText } from "@/components/ui/animated-gradient-text";
+import { cn } from "@/lib/utils";
+import { RainbowButton } from "@/components/ui/rainbow-button";
+import { FlickeringGrid } from "@/components/ui/flickering-grid";
+
 
 import { useState } from "react";
 export default function Home() {
@@ -25,49 +29,57 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <div className="min-h-screen bg-[#310D4D]">
+      <header className="sticky top-0 z-20 flex items-center justify-center p-4 backdrop-blur-[2px] border-b border-gray-800">
+        <img src="/temp_logo.png" alt="Fanfantasy.ai Logo" className="h-8" />
+      </header>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center text-center z-10">
+          <AnimatedGradientText>
+            🎉 <hr className="mx-2 h-4 w-px shrink-0 bg-transparent" />{" "}
+            <span
+              className={cn(
+                `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`,
+              )}
+            >
+              Introducing fanfantasy.ai
+            </span>
+          </AnimatedGradientText>
+          <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl text-white">
+            your fantasy, realized.
+          </h1>
 
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-        {success ? (
-          <p className="">Thank you for signing up!</p>
-        ) : (
-          <form onSubmit={handleSubmit} className="">
-            <input
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className=""
-            />
-            <button type="submit" className="">
-              Join Waitlist
-            </button>
-          </form>
-        )}
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <div className="flex gap-4 items-center flex-col">
+            {success ? (
+              <p className="text-white">Thank you for signing up!</p>
+            ) : (
+              <form onSubmit={handleSubmit} className="bg-transparent border border-gray-400 py-2 pl-4 pr-2 rounded-2xl">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                  className="bg-transparent outline-none text-white w-[250px]"
+                />
+                <RainbowButton>Join waitlist</RainbowButton>
+              </form>
+            )}
+          </div>
+        </main>
+        <div className="fixed inset-0">
+          <FlickeringGrid
+            className="absolute inset-0 z-0 size-full"
+            squareSize={1}
+            gridGap={6}
+            color="#6B7280"
+            maxOpacity={0.8}
+            flickerChance={0.1}
+            height={1200}
+            width={1920}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-
-      </footer>
+      </div>
     </div>
   );
 }
